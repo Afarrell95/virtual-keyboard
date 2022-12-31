c5ca252aec92a375bf06049c0e4edf5f7aaadbb0
Allison01~ <farrella95@gmail.com>
2022-12-30 20:54:17 -0500

    js start

---
+++ app.js
@@ -0,0 +1,42 @@
+const Keyboard = {
+    elements: {
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
+
+    eventHandlers: {
+        oninput: null,
+        onclose: null
+    },
+
+    properties: {
+        value: '',
+        capslock: false
+    },
+
+init() {
+
+},
+
+createKeys() {
+
+},
+
+triggerEvents() {
+
+},
+
+toggleCapsLock() {
+    
+},
+
+open() {
+
+},
+
+close() {
+    
+}
+
+}

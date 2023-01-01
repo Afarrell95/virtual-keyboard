6c6dd87251700718833034e507cc74e6c44054fb
Allison <farrella95@gmail.com>
2023-01-01 14:13:52 -0500

    more js

--- app.js
+++ app.js
@@ -1,42 +1,111 @@
 const Keyboard = {
-    elements: {
-        main: null,
-        keysContainer: null,
-        keys: []
-    },
+  elements: {
+    main: null,
+    keysContainer: null,
+    keys: [],
+  },
 
-    eventHandlers: {
-        oninput: null,
-        onclose: null
-    },
+  eventHandlers: {
+    oninput: null,
+    onclose: null,
+  },
 
-    properties: {
-        value: '',
-        capslock: false
-    },
+  properties: {
+    value: "",
+    capslock: false,
+  },
 
-init() {
+  init() {
+    //create main elements
+    this.elements.main = document.createElement("div");
+    this.elements.keysContainer = document.createElement("div");
 
-},
+    //set up main elements
+    this.elements.main.classList.add("keyboard", "1keyboard--hidden");
+    this.elements.keysContainer.classList.add("keys");
 
-createKeys() {
+    //add to DOM
+    this.elements.main.appendChild(this.elements.keysContainer);
+    document.body.appendChild(this.elements.main);
+  },
 
-},
+  createKeys() {
+    //returns document fragment
+    const fragment = document.createDocumentFragment();
+    const keyLayout = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "0",
+      "backspace",
+      "q",
+      "w",
+      "e",
+      "r",
+      "t",
+      "y",
+      "u",
+      "i",
+      "o",
+      "p",
+      "caps",
+      "a",
+      "s",
+      "d",
+      "f",
+      "g",
+      "h",
+      "j",
+      "k",
+      "l",
+      "enter",
+      "done",
+      "z",
+      "x",
+      "c",
+      "v",
+      "b",
+      "n",
+      "m",
+      ",",
+      ".",
+      "?",
+      "space",
+    ];
 
-triggerEvents() {
+    //creates html for an icon
 
-},
+    // const creatIcon = (icon_name) => {
+    //     return
+    // }
 
-toggleCapsLock() {
-    
-},
+    keyLayout.forEach((key) => {
+      const KeyElement = document.createElement("button");
+      const lineBreak = ["backspace", "p", "enter", "?"].indexOf(key) !== -1;
 
-open() {
+      //add attributes/classes
+    });
+  },
 
-},
+  triggerEvents(handlerName) {
+    console.log("event triggered!" + handlerName);
+  },
 
-close() {
-    
-}
+  toggleCapsLock() {
+    console.log("caps lock toggled");
+  },
 
-}
+  open(initialValue, oninput, onclose) {},
+
+  close() {},
+};
+
+window.addEventListener("DOMContentLoaded", function () {
+  Keyboard.init();
+});

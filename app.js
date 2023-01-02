d116f7ba04e3569dc6dcd14be7743717783f5eaa
Allison <farrella95@gmail.com>
2023-01-01 21:13:27 -0500

    redo

--- app.js
+++ app.js
@@ -1,111 +1,31 @@
-const Keyboard = {
-  elements: {
-    main: null,
-    keysContainer: null,
-    keys: [],
-  },
+const buttons = document.querySelectorAll(".btn");
+const textarea = document.querySelector("textarea");
 
-  eventHandlers: {
-    oninput: null,
-    onclose: null,
-  },
+const delete_btn = document.querySelector(".delete");
+const shift_btn = document.querySelector(".shift");
+const space_btn = document.querySelector(".space");
 
-  properties: {
-    value: "",
-    capslock: false,
-  },
+let chars = [];
 
-  init() {
-    //create main elements
-    this.elements.main = document.createElement("div");
-    this.elements.keysContainer = document.createElement("div");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    textarea.value += btn.innerText;
+    chars = textarea.value.split("");
+  });
+});
 
-    //set up main elements
-    this.elements.main.classList.add("keyboard", "1keyboard--hidden");
-    this.elements.keysContainer.classList.add("keys");
+delete_btn.addEventListener("click", () => {
+  chars.pop();
+  textarea.value = chars.join("");
+});
 
-    //add to DOM
-    this.elements.main.appendChild(this.elements.keysContainer);
-    document.body.appendChild(this.elements.main);
-  },
+space_btn.addEventListener("click", () => {
+  chars.push(" ");
+  textarea.value = chars.join("");
+});
 
-  createKeys() {
-    //returns document fragment
-    const fragment = document.createDocumentFragment();
-    const keyLayout = [
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "0",
-      "backspace",
-      "q",
-      "w",
-      "e",
-      "r",
-      "t",
-      "y",
-      "u",
-      "i",
-      "o",
-      "p",
-      "caps",
-      "a",
-      "s",
-      "d",
-      "f",
-      "g",
-      "h",
-      "j",
-      "k",
-      "l",
-      "enter",
-      "done",
-      "z",
-      "x",
-      "c",
-      "v",
-      "b",
-      "n",
-      "m",
-      ",",
-      ".",
-      "?",
-      "space",
-    ];
-
-    //creates html for an icon
-
-    // const creatIcon = (icon_name) => {
-    //     return
-    // }
-
-    keyLayout.forEach((key) => {
-      const KeyElement = document.createElement("button");
-      const lineBreak = ["backspace", "p", "enter", "?"].indexOf(key) !== -1;
-
-      //add attributes/classes
-    });
-  },
-
-  triggerEvents(handlerName) {
-    console.log("event triggered!" + handlerName);
-  },
-
-  toggleCapsLock() {
-    console.log("caps lock toggled");
-  },
-
-  open(initialValue, oninput, onclose) {},
-
-  close() {},
-};
-
-window.addEventListener("DOMContentLoaded", function () {
-  Keyboard.init();
+shift_btn.addEventListener("click", () => {
+  buttons.forEach((btn) => {
+    btn.classList.toggle("upper");
+  });
 });
